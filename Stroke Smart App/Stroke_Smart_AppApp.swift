1c7faaf43fb7b0b807f06f6e0c0aa1d6686c767e
Ryan Blanco <ryblan@amazon.com>
2024-07-06 22:43:47 -0700

    Initial Commit

---
+++ Stroke Smart App/Stroke_Smart_AppApp.swift
@@ -0,0 +1,32 @@
+//
+//  Stroke_Smart_AppApp.swift
+//  Stroke Smart App
+//
+//  Created by Blanco, Ryan on 7/6/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Stroke_Smart_AppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Stroke Smart AppUITests/Stroke_Smart_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Stroke_Smart_AppUITestsLaunchTests.swift
+//  Stroke Smart AppUITests
+//
+//  Created by Blanco, Ryan on 7/6/24.
+//
+
+import XCTest
+
+final class Stroke_Smart_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
